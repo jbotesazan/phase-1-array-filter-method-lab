3c6a6fbde09274e1855b2a7a8cf5d93cbaec5ef4
Jason Botesazan <jbotesazan@gmail.com>
2021-11-29 13:06:02 -0500

    commit tree 9c6fa6824befebe91be19a8c2806a5921b5ad61e

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching(item, driver) {
+
+    const filt = item.filter(function (name) {
+          return name.toLowerCase() === driver.toLowerCase()
+     });
+     return filt;
+  
+  }
+  
+
+function fuzzyMatch(item, string) {
+    
+    const filta = item.filter(function (name) {
+           return (name.startsWith(string))       
+    });
+    return filta
+}
+
+function matchName(item, string) {
+   return item.filter(function (element) {
+        return   element.name === string
+    })
+}
